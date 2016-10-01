89b4ceafabae749c71e0a6e8b1583c18b11b2960
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2016-09-30 21:58:04 -0700

    feat (code) add default implementation for uitableviewcell for resuableview protocol

---
+++ project-one-storm-viewer/storm viewer/storm viewer/UITableViewCell+ReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  UITableViewCell+ReusableView.swift
+//  storm viewer
+//
+//  Created by Gregory Hutchinson on 9/30/16.
+//  Copyright © 2016 Das Hutch Development. All rights reserved.
+//
+// https://www.natashatherobot.com/swift-3-0-refactoring-cues/
+
+import Foundation
+import  UIKit
+
+extension UITableViewCell: ReusableView {}
