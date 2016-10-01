5507c70109ef398e3eb02e17dae3b2681bf7517e
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2016-10-01 12:57:36 -0700

    chore (Xcode) initial project setup and organization

---
+++ project-four-easy-browser/easy browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  easy browser
+//
+//  Created by Gregory Hutchinson on 10/1/16.
+//  Copyright © 2016 Das Hutch Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
