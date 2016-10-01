2f4fcc080605f35679f7b481934592e4bf96fe32
Gregory Hutchinson <mr.gregoryhutchinson@gmail.com>
2016-09-30 22:14:44 -0700

    feat (code) initial app setup and adjustments after Xcode default project creation

---
+++ project-two-guess-the-flag/guess the flag/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  guess the flag
+//
+//  Created by Gregory Hutchinson on 9/30/16.
+//  Copyright © 2016 Das Hutch Development. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ project-two-guess-the-flag/guess the flag/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  guess the flag
+//
+//  Created by Gregory Hutchinson on 9/30/16.
+//  Copyright © 2016 Das Hutch Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        config()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+//MARK: - Public
+
+//MARK: - Private
+    private func config() {
+
+    }
+}
+
